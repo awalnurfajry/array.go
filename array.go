220c1a24125fc7fd5f7d41ce952178ab766f079a
Muh. awal nurfajry <95205328+awalnurfajry@users.noreply.github.com>
2022-01-26 20:49:58 +0800

    Add files via upload

---
+++ array.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var names [3]string
+
+	names[0] = "Muh"
+	names[1] = "Awal"
+	names[2] = "Nurfajry"
+
+	fmt.Println(names[0])
+	fmt.Println(names[1])
+	fmt.Println(names[2])
+
+}
